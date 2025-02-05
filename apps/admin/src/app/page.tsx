e6d7ef4496c37634ef0f426b1d7328a3dc4e2d98
Le Hong Phong <phonghngle@gmail.com>
2025-02-05 10:16:00 +0700

    initialize nextjs app

---
+++ apps/admin/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@mui/material'
+
+export default function Home() {
+  return (
+    <div className="admin">
+      <h1>Admin</h1>
+      <p>Welcome to admin!</p>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </div>
+  )
+}
--- packages/client/src/app/products/index.tsx
+++
@@ -1,6 +0,0 @@
-import useDocumentTitle from "../../hooks/title";
-
-export function Home() {
-  useDocumentTitle("Website Title For Home Page");
-  return <h1>Home Page</h1>;
-}
