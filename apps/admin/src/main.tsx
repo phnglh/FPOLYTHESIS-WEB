1eb9b43ece1e7a3bcef22fe87ac891207df92379
Le Hong Phong <phonghngle@gmail.com>
2025-02-20 10:54:58 +0700

    update ErrorFallback component

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -5,15 +5,16 @@ import { BrowserRouter } from 'react-router'
 import { ErrorBoundary } from 'react-error-boundary'
 import './config/i18n.ts'
 import './styles/global.css'
+import ErrorFallback from './layout/components/ErrorFallback.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <ErrorBoundary FallbackComponent={ErrorFallback}> */}
-    <Suspense fallback={<div>Loading...</div>}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Suspense>
-    {/* </ErrorBoundary> */}
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Suspense>
+    </ErrorBoundary>
   </StrictMode>,
 )
