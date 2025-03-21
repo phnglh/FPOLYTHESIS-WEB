287c2720fc6951e4d135667caa4b92ed1eaacc43
Le Hong Phong <phonghngle@gmail.com>
2025-03-21 15:33:06 +0700

    chore: import useLocation

--- apps/admin/src/layout/components/SideBar.tsx
+++ apps/admin/src/layout/components/SideBar.tsx
@@ -1,5 +1,6 @@
 import { items } from '@layout/components/common/SideBarItem'
 import { Menu } from 'antd'
+import { useLocation } from 'react-router'
 
 export function Sidebar() {
   const location = useLocation()
